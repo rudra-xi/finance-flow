abbdfaa89248a860cd7b4fb6220a3804b6f154f5
rudra-xi <goutam.prasad.2707@gmail.com>
2025-02-16 22:40:18 +0530

    Switch from BrowserRouter to HashRouter for improved routing compatibility

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // Change BrowserRouter to HashRouter
 import "./index.css";
 import App from "./App.jsx";
 import AppContextProvider from "./Context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
+	<HashRouter>
 		<AppContextProvider>
 			<App />
 		</AppContextProvider>
-	</BrowserRouter>
+	</HashRouter>
 );
