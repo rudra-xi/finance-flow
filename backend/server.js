9d5d7be115be8a1dfade7d4f0f25cee68560ddf8
rudra-xi <goutam.prasad.2707@gmail.com>
2025-02-16 20:10:57 +0530

    cors causing problem while deploying

--- backend/server.js
+++ backend/server.js
@@ -12,24 +12,40 @@ const PORT = process.env.PORT || 5000; // Set port
 
 const server = express();
 
+// Enable CORS for specific origins
 server.use(
 	cors({
-		origin: [process.env.RENDER_FRONTEND_URL, process.env.FRONTEND_URL], // Allow only these origins
-		credentials: true, // If using cookies or authentication headers
+		origin: [
+			"https://finance-flow-xuyt.onrender.com",
+			"http://localhost:3000",
+		],
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
 	})
 );
+
 server.use(express.json()); // Parse JSON
 
 database(); // Connect to database
+
+// Handle preflight requests
+server.options("*", cors());
+
+// Define routes
+server.use("/auth", authRoute); // http://finance-flow-backend.onrender.com/auth/
+server.use("/budget", budgetRoute); // http://finance-flow-backend.onrender.com/budget/
+server.use("/expense", expenseRoute); // http://finance-flow-backend.onrender.com/expense/
 
 server.get("/", (req, res) => {
 	res.send("Backend Running");
 });
 
-// Define routes
-server.use("/endpoint/auth", authRoute); //http://127.0.0.1:5000/endpoint/auth/
-server.use("/endpoint/budget", budgetRoute); //http://127.0.0.1:5000/endpoint/budget/
-server.use("/endpoint/expense", expenseRoute); //http:127.0.0.1:5000/endpoint/expense/
+//! 
+// // Define routes
+// server.use("/endpoint/auth", authRoute); //http://127.0.0.1:5000/endpoint/auth/
+// server.use("/endpoint/budget", budgetRoute); //http://127.0.0.1:5000/endpoint/budget/
+// server.use("/endpoint/expense", expenseRoute); //http:127.0.0.1:5000/endpoint/expense/
 
 server.listen(PORT, () => {
 	console.log(`Server is Running on http://localhost:${PORT}`);
