7c10c80db6132ead6d76a406d92ba0ad4ca0087e
rudra <goutam.prasad.2707@gmail.com>
2025-11-06 21:37:05 +0530

    Fix: Move padding on the top to App container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import { Budget, Expense, About, SignIn } from "./Pages";
-import { Navbar, Footer } from "./Components";
+import { Route, Routes } from "react-router-dom";
+import { Footer, Navbar } from "./Components";
+import { About, Budget, Expense, SignIn } from "./Pages";
 
 function App() {
-	return (
-		<div className="bg-primary px-5 pt-3 text-secondary min-h-screen font-inter">
-			<Toaster position="top-center" reverseOrder={false} />
-			<Navbar />
+  return (
+    <div className="bg-primary px-5 pt-28 text-secondary min-h-screen font-inter">
+      <Toaster position="top-center" reverseOrder={false} />
+      <Navbar />
 
-			<Routes>
-				<Route path="/" element={<SignIn />} />
-				<Route path="/budget" element={<Budget />} />
-				<Route path="/expense" element={<Expense />} />
-				<Route path="/about" element={<About />} />
-			</Routes>
-			<Footer />
-		</div>
-	);
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/budget" element={<Budget />} />
+        <Route path="/expense" element={<Expense />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
